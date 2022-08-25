97a99f168637d8680d9488c51fd15b8c571215e5
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-08-25 21:20:07 +0300

    addTask41AndTask43

---
+++ task41/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+System.Console.Clear();
+
+int NumbersGreaterThanZero(int amountOfNumbers)
+{
+    int count = 0;
+    int userNumber = 0;
+    while (amountOfNumbers > 0)
+    {
+        System.Console.WriteLine("Введите число для проверки");
+        userNumber = int.Parse(Console.ReadLine()!);
+        if (userNumber > 0)
+        {
+            count++;
+        }
+        amountOfNumbers--;     
+    }
+    return count;
+}
+
+
+
+
+System.Console.WriteLine("Какое количество чисел вы хотите проверить");
+int amountOfNumbers = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine($"Количество чисел больше 0 - {NumbersGreaterThanZero(amountOfNumbers)}");
---
+++ task43/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+System.Console.Clear();
+
+System.Console.WriteLine("Введите начало отрезка b1");
+double b1 = double.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите конец отрезка b2");
+double b2 = double.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите начало отрезка k1");
+double k1 = double.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите конец отрезка k2");
+double k2 = double.Parse(Console.ReadLine()!);
+
+double x = b1 / -b2;
+double y = k2 * x + b2;
+
+Console.Write($"Координата точки пересечения ({x} ; {y})");
